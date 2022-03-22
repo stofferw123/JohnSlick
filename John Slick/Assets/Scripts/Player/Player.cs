89566f0f5fc4d98366c2494b78592c9df977d64c
Mikkel Meiling <mikkelwnm@gmail.com>
2022-03-22 10:27:29 +0100

    plz

--- John Slick/Assets/Scripts/Player/Player.cs
+++ John Slick/Assets/Scripts/Player/Player.cs
@@ -5,8 +5,29 @@ using UnityEngine.Events;
 
 public class Player : MonoBehaviour, IAgent
 {
-    public int health { get; set; }
+
+    [SerializeField]
+    private int maxHealth;
+    [SerializeField]
+    private int health;
+    private int Health { get => health; set 
+        {
+            Health = Mathf.Clamp(value, 0, maxHealth);
+            uiHealth.UpdateUI(health);
+        } 
+    }
+
+    [field: SerializeField]
+    public UIHealth uiHealth { get; set; }
 
     public UnityEvent OnDie { get; set; }
     public UnityEvent OnGetHit { get; set; }
+
+    int IAgent.health => throw new System.NotImplementedException();
+
+    private void Start()
+    {
+        health = maxHealth;
+        uiHealth.Initialize(health);
+    }
 }
